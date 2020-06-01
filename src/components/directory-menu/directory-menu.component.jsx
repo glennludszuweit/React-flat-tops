739e53178c6534f8ab1e318a891f2cca22853620
glenn <glenn.ludszuweit@gmail.com>
2020-06-01 19:55:31 +0200

    added dev datas

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
+
+import SECTIONS_DATA from "../../sections.data";
 import "./directory-menu.styles.scss";
 
 class DirectoryMenu extends React.Component {
@@ -7,46 +9,15 @@ class DirectoryMenu extends React.Component {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
+      sections: SECTIONS_DATA,
     };
   }
 
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
