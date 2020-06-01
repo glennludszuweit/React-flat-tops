97b9f12337aa772aa81efdab6143fdd471772c8c
glenn <glenn.ludszuweit@gmail.com>
2020-06-01 16:07:47 +0200

    refactored homepage and setup folder and file structure

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DirectoryMenu from "../../components/directory-menu/directory-menu.component";
+import "./homepage.styles.scss";
+
+const HomPage = () => (
+  <div className="hompage">
+    <DirectoryMenu />
+  </div>
+);
+
+export default HomPage;
