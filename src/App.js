217442c359ab6094368c0266924bcd7f6a7d4c7b
glenn <glenn.ludszuweit@gmail.com>
2020-06-01 20:00:59 +0200

    setup routes to all sections

--- src/App.js
+++ src/App.js
@@ -10,12 +10,40 @@ const HatsPage = () => (
   </div>
 );
 
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+);
+
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+);
+
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+);
+
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop/jackets" component={JacketsPage} />
+        <Route exact path="/shop/sneakers" component={SneakersPage} />
+        <Route exact path="/shop/womens" component={WomensPage} />
+        <Route exact path="/shop/mens" component={MensPage} />
       </Switch>
     </div>
   );
