a5ca9c8307d741825c9e9366c969cea767c8bfab
glenn <glenn.ludszuweit@gmail.com>
2020-06-02 14:48:02 +0200

    build custom button component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./form-input.styles.scss";
 
-const FormInput = ({ handleChange, label, ...otherFormInputProps }) => (
+const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className="group">
-    <input
-      className="form-input"
-      onChange={handleChange}
-      {...otherFormInputProps}
-    />
+    <input className="form-input" onChange={handleChange} {...otherProps} />
     {label ? (
       <label
         className={`${
-          otherFormInputProps.value.length ? "shrinkLabel" : ""
+          otherProps.value.length ? "shrink" : ""
         } form-input-label`}
       >
         {label}
