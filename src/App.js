317dec86b9e15094c7f1965a008e196ae2a68da1
glenn <glenn.ludszuweit@gmail.com>
2020-06-04 11:07:51 +0200

    make shop page mobile responsive

--- src/App.js
+++ src/App.js
@@ -9,36 +9,6 @@ import AuthPage from "./pages/authpage/authpage.component";
 
 import Header from "./components/header/header.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-);
-
-const SneakersPage = () => (
-  <div>
-    <h1>SNEAKERS PAGE</h1>
-  </div>
-);
-
-const WomensPage = () => (
-  <div>
-    <h1>WOMENS PAGE</h1>
-  </div>
-);
-
-const MensPage = () => (
-  <div>
-    <h1>MENS PAGE</h1>
-  </div>
-);
-
 class App extends React.Component {
   constructor() {
     super();
@@ -81,12 +51,6 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
           <Route exact path="/auth" component={AuthPage} />
-
-          <Route exact path="/shop/hats" component={HatsPage} />
-          <Route exact path="/shop/jackets" component={JacketsPage} />
-          <Route exact path="/shop/sneakers" component={SneakersPage} />
-          <Route exact path="/shop/womens" component={WomensPage} />
-          <Route exact path="/shop/mens" component={MensPage} />
         </Switch>
       </div>
     );
