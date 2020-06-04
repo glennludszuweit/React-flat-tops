5bb9befef4c7fbee248f4d038a977411feefcae4
glenn <glenn.ludszuweit@gmail.com>
2020-06-04 18:05:16 +0200

    created cart-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import "./header.styles.scss";
 
@@ -31,6 +32,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
